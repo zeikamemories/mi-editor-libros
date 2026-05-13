c13056446688397796ba688c5d2423424f9d975e
sacem069 <sacem069@newschool.edu>
2026-05-13 16:26:04 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Zeika',
   description: 'Zeika Memories & Editorial',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
